b1eec2a1e19c67cace9ce3ad5f2649b254e16529
thiagohrcosta <thiagohrcosta86@gmail.com>
2022-04-23 22:55:00 -0300

    adding style to inputs and images

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -3,31 +3,34 @@ import { KeyboardAvoidingView, Platform } from 'react-native'
 
 import { Input } from '@components/Input';
 
-import { Container } from './styles';
+import { Container, Content, Title } from './styles';
 import { Button } from '@components/Button';
 
 export function SignIn() {
   return (
     <Container>
       <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : undefined }>
-        <Input
-          placeholder='E-mail'
-          type="secondary"
-          autoCorrect={false}
-          autoCapitalize="none"
-        />
-        <Input
-          placeholder='Senha'
-          type="secondary"
-          secureTextEntry
-          autoCorrect={false}
-          autoCapitalize="none"
-        />
+        <Content>
+          <Title>Login</Title>
+          <Input
+            placeholder='E-mail'
+            type="secondary"
+            autoCorrect={false}
+            autoCapitalize="none"
+          />
+          <Input
+            placeholder='Senha'
+            type="secondary"
+            secureTextEntry
+            autoCorrect={false}
+            autoCapitalize="none"
+          />
 
-        <Button
-          title="Entrar"
-          type="secondary"
-        />
+          <Button
+            title="Entrar"
+            type="secondary"
+          />
+        </Content>
       </KeyboardAvoidingView>
     </Container>
   );
